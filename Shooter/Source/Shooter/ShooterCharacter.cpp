8db8da99c235df4ac583460d1d427bc45d79278b
Alfredo Sosa <alfreSosa@users.noreply.github.com>
2021-03-04 19:14:48 +0100

    modify rotation 45

--- Shooter/Source/Shooter/ShooterCharacter.cpp
+++ Shooter/Source/Shooter/ShooterCharacter.cpp
@@ -78,7 +78,8 @@ AShooterCharacter::AShooterCharacter()
 	VR_MuzzleLocation = CreateDefaultSubobject<USceneComponent>(TEXT("VR_MuzzleLocation"));
 	VR_MuzzleLocation->SetupAttachment(VR_Gun);
 	VR_MuzzleLocation->SetRelativeLocation(FVector(0.000004, 53.999992, 10.000000));
-	VR_MuzzleLocation->SetRelativeRotation(FRotator(0.0f, 90.0f, 0.0f));		// Counteract the rotation of the VR gun model.
+	// modify rotation
+	VR_MuzzleLocation->SetRelativeRotation(FRotator(0.0f, 45.0f, 0.0f));		// Counteract the rotation of the VR gun model.
 
 	// Uncomment the following line to turn motion controllers on by default:
 	//bUsingMotionControllers = true;
